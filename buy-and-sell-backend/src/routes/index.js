92a993f2d387b2bf926084a35e1aea7f89148cc9
cynthiadai122 <sdai10@student.monash.edu>
2020-09-16 02:10:59 +1000

    mysql-01

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -1,8 +1,10 @@
+import { addViewToListingRoute} from './addViewToListing';
 import { getAllListingsRoute } from './getAllListings';
-import {getListingRoute} from './getListing';
+import { getListingRoute} from './getListing';
 
 
 export default[
+    addViewToListingRoute,
     getAllListingsRoute,
     getListingRoute,
     
