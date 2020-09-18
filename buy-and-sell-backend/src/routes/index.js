c79116203918a5c831fbe95d64f89207427798c2
cynthiadai122 <sdai10@student.monash.edu>
2020-09-18 20:59:55 +1000

    finish backend add,update,delete

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -1,11 +1,19 @@
-import { addViewToListingRoute} from './addViewToListing';
+import { addViewToListingRoute } from './addViewToListing';
+import { createNewListingRoute } from './createNewListing';
 import { getAllListingsRoute } from './getAllListings';
-import { getListingRoute} from './getListing';
+import { getListingRoute } from './getListing';
+import { getUserListingsRoute } from './getUserListings';
+import { updateListingRoute } from './updateListing';
+import { deleteListingRoute } from './deleteListing';
 
 
-export default[
+export default [
     addViewToListingRoute,
+    createNewListingRoute,
     getAllListingsRoute,
     getListingRoute,
-    
+    getUserListingsRoute,
+    updateListingRoute,
+    deleteListingRoute,
+
 ];
